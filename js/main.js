763ada29a635e01d6f05c9c135c9f58556d1bd3f
JairoRC05 <64911345+JairoRC05@users.noreply.github.com>
2021-01-07 14:22:16 -0600

    zip

--- js/main.js
+++ js/main.js
@@ -31,3 +31,27 @@ window.addEventListener('click',function(e){
         }
     }
 });
+
+
+
+
+   // Look for .hamburger
+        var hamburger = document.querySelector(".hamburger");
+        // On click
+        hamburger.addEventListener("click", function () {
+            // Toggle class "is-active"
+            hamburger.classList.toggle("is-active");
+            // Do something else, like open/close menu
+        });
+
+
+
+//Navbar
+        $(window).scroll(function () {
+            $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+        });
+
+
+
+        //AOS
+         AOS.init();
