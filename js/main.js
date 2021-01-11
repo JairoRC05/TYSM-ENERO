3dc3fa6cd86937d54849b808bf7754017cee24ca
JairoRC05 <64911345+JairoRC05@users.noreply.github.com>
2021-01-11 15:35:53 -0600

    icon ins

--- js/main.js
+++ js/main.js
@@ -4,11 +4,6 @@
             $("#navbar").toggleClass('scrolled', $(this).scrollTop() > 50);
             $("#text_dark").removeClass('text-dark');
         });
-
-       
-
-     
-
         //AOS
          AOS.init();
 
