ecd46e4691487657275e7b163a0fb69f870d5af3
JairoRC05 <64911345+JairoRC05@users.noreply.github.com>
2021-01-11 06:30:11 -0600

    version

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@
 //Navbar
         $(window).scroll(function () {
             $("#navbar").toggleClass('scrolled', $(this).scrollTop() > 50);
+            $("#text_dark").removeClass('text-dark');
         });
 
+       
+
+     
 
         //AOS
          AOS.init();
