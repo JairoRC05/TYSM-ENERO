70684e9d4c8dab378c9eb092d3abe8fe08f81a41
JairoRC05 <64911345+JairoRC05@users.noreply.github.com>
2021-01-11 19:15:01 -0600

    version

--- js/main.js
+++ js/main.js
@@ -9,6 +9,29 @@
 
 
 
+  $('document').ready(function () {
+            $('#form').on('submit', function (event) {
+                event.preventDefault();
 
+                var formData = new FormData(this);
+                formData.append('service_id', 'gsoftware');
+                formData.append('template_id', 'contact');
+                formData.append('user_id', 'user_HtJj99X5RdXnEqxLAburM');
+
+
+                $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+                    type: 'POST',
+                    data: formData,
+                    contentType: false,
+                    processData: false
+                }).done(function () {
+                    alert('! Email Enviado Existosa mente !');
+                    consoloe.log('Enviado');
+                }).fail(function (error) {
+                    alert('Error ... ' + JSON.stringify(error));
+                });
+            });
+
+        });
       
 
